37d641b1f61d3e4f6dd11f15b415aa4fe5b91e4b
Igor de Alcantara Barroso <idabmat@gmail.com>
2018-02-10 18:43:40 +0100

    Bump patch version

--- mix.exs
+++ mix.exs
@@ -4,10 +4,10 @@ defmodule TicTacToe.Mixfile do
   def project do
     [
       app: :tic_tac_toe,
-      version: "1.1.2",
+      version: "1.1.3",
       description: "A TicTacToe game engine",
       package: package(),
-      elixir: "~> 1.5",
+      elixir: ">= 1.5",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       source_url: "https://github.com/idabmat/tic_tac_toe",
