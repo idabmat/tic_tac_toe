4d94d6fc5bce7c8f396b0030ed42d5eece5df77d
Igor de Alcantara Barroso <idabmat@gmail.com>
2018-02-10 18:49:14 +0100

    fix elixir version

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule TicTacToe.Mixfile do
       version: "1.1.3",
       description: "A TicTacToe game engine",
       package: package(),
-      elixir: ">= 1.5",
+      elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       source_url: "https://github.com/idabmat/tic_tac_toe",
